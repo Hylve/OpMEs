1e20e7043951d2b9923717628e6a9c93fe200a53
Christoffer <Hylve@users.noreply.github.com>
2017-08-24 23:47:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -21,6 +21,18 @@ class node(object):
 def main():
     node1=nodeModule.node("1","NodeOne","Node",["in1","in2"],["out1"])
     print node1
+    # Below is for testing for testing
+    print(node1.getName())
+    node1.changeName("NewNodeName")
+    print node1
+    node1.addInFlow("new inflow")
+    print node1
+    node1.addOutFlow("new outflow")
+    print node1
+
+    a=node1.getAll()
+    #print a
+    print type(a)
 
 
 
