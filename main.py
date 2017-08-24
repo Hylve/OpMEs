b4a257dbc3edb82a678b35d3946f1c0e21812ecc
Christoffer <Hylve@users.noreply.github.com>
2017-08-25 00:03:28 +0200

    Added some comments

--- main.py
+++ main.py
@@ -19,23 +19,26 @@ class node(object):
 
 
 def main():
+    #create new node
     node1=nodeModule.node("1","NodeOne","Node",["in1","in2"],["out1"])
+    #print new node
     print node1
-    # Below is for testing for testing
-    print(node1.getName())
-    node1.changeName("NewNodeName")
-    print node1
-    node1.addInFlow("new inflow")
-    print node1
-    node1.addOutFlow("new outflow")
-    print node1
+    # Below is for testing 
+    print(node1.getName())#prints the name of the node
+    node1.changeName("NewNodeName")# changes the node name
+    print node1 # print node
+    node1.addInFlow("new inflow") # adds a new flow
+    print node1 #print node
+    node1.addOutFlow("new outflow") # adds a new flow
+    print node1 #print node
 
-    a=node1.getAll()
-    #print a
-    print type(a)
+    a=node1.getAll() # recive all data from node
+    #print a 
+    print type(a) #print type (tuple)
 
 
 
 
 if __name__ == '__main__':
+    #if this is the main-file run main function
     main()
