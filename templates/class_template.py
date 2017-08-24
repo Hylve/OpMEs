3a50d04c7d48b34acce667dbfd432c36e8f802d3
Christoffer <Hylve@users.noreply.github.com>
2017-08-24 21:58:46 +0200

    Create class_template.py

---
+++ templates/class_template.py
@@ -0,0 +1,24 @@
+'''
+Name:
+Desc:
+Author:
+Version:
+'''
+
+class node(object):
+    """docstring for this class """
+    # Constructor
+    def __init__(self):
+
+    # Accessers methods (Getters)
+
+    #Mutator methods (Setters)
+
+
+
+def main():
+    
+
+
+if __name__ == '__main__':
+    main()
