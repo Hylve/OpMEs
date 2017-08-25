ff12eeeb295406c9380f0dc0d001565e0138f399
Christoffer <Hylve@users.noreply.github.com>
2017-08-25 13:59:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
+#!/usr/bin/python3
 '''
 Name: main.py
-Desc: Main
-Author: Chris [ec@ecn.se]
-Version: 0.0.0.1
+Desc:
+Author: Chris [ecn@ecn.se]
+Version: 0.0.0.2
 '''
-import nodeModule
+import nodeModule # node data
+import setupModule #
+import sys
 
 
 
-class node(object):
+
+
+class mainClass(object):
     """docstring for this class """
     # Constructor
     def __init__(self):
@@ -19,26 +24,8 @@ class node(object):
 
 
 def main():
-    #create new node
-    node1=nodeModule.node("1","NodeOne","Node",["in1","in2"],["out1"])
-    #print new node
-    print node1
-    # Below is for testing 
-    print(node1.getName())#prints the name of the node
-    node1.changeName("NewNodeName")# changes the node name
-    print node1 # print node
-    node1.addInFlow("new inflow") # adds a new flow
-    print node1 #print node
-    node1.addOutFlow("new outflow") # adds a new flow
-    print node1 #print node
-
-    a=node1.getAll() # recive all data from node
-    #print a 
-    print type(a) #print type (tuple)
-
-
-
+    print ("Python version: "+str(sys.version_info[0]))
+    setupModule.setupModel() #
 
 if __name__ == '__main__':
-    #if this is the main-file run main function
     main()
