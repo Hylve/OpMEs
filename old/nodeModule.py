9ab95e607fe5e59f5e513e5cc28d5322becb47a0
Hylve <ecn@ecn.se>
2017-09-15 09:50:07 +0200

    Update

---
+++ old/nodeModule.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python3
+'''
+Name: nodeModule.py
+Desc: The node class
+Author: Chris[ecn@ecn.se],
+Version: 0.0.0.2
+'''
+import main             #
+#import setupModule      #
+# import newNodeModule    #
+
+
+class node(model):
+    """
+    docstring for this class
+        Attributes:
+            id:
+            namedescription:
+            type:
+            inGoingFlows:
+            outGoingFlows:
+    """
+    # Constructor
+    # def __init__(self, id, namedescription,type,inGoingFlows,outGoingFlows):
+    #     self.nodeId = id
+    #     self.nodeName = namedescription
+    #     self.nodeType= type
+    #     self.nodeInFlows = inGoingFlows
+    #     self.nodeOutFlows = outGoingFlows
+    def __init__(self):
+        pass
+    # Accessers methods (Getters)
+    def getAll(self):
+        return self.nodeId,self.nodeName,self.nodeType,self.nodeInFlows,self.nodeOutFlows
+    def getId(self):
+        return self.nodeId
+    def getName(self):
+        return self.nodeName
+    def getType(self):
+        return self.nodeType
+    def getInFlows(self):
+        return self.nodeInFlows
+    def getOutFlows(self):
+        return self.nodeOutFlows
+    # Mutator methods (Setters)
+    def changeName(self, newName):
+        self.nodeName = newName
+    def addInFlow(self, inFlowToAdd):
+        self.nodeInFlows.append(inFlowToAdd)
+    def addOutFlow(self, outFlowToAdd):
+        self.nodeOutFlows.append(outFlowToAdd)
+    # remove inflow function
+    # remove outflow function
+
+    def __str__(self):
+        '''
+        Move string converter? Is it needed in the future?
+        '''
+        if type(self.nodeInFlows) is list:
+            inFlowstr = ''
+            for element in self.nodeInFlows:
+                inFlowstr += str(element)+";"
+            #return inFlowstr
+        if type(self.nodeOutFlows) is list:
+            outFlowstr = ''
+            for element in self.nodeOutFlows:
+                outFlowstr += str(element)+";"
+
+        return self.nodeId+";"+self.nodeName+";"+self.nodeType+";"+inFlowstr+outFlowstr
+
+
+
+
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
---
+++ old/setupModule.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+'''
+Name:
+Desc:
+Author: Chris [ecn@ecn.se]
+Version:0.0.0.1
+'''
+import main             #
+import nodeModule       # node data
+import newNodeModule    #
+
+class setupModel(object):
+    """docstring for this class """
+    # Constructor
+    def __init__(self):
+        # Create all that is needed
+        self.nodesList = {} # This list contains...
+        print("setupModel - init")
+    # Accessers methods (Getters)
+    def getNodeList(self):
+        return self.nodesList
+
+    # Mutator methods (Setters)
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
