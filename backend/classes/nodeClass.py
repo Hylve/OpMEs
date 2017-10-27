fb429ab731ccfc9bb5d471fdd9c26a773e116420
Hylve <ecn@ecn.se>
2017-10-27 10:48:51 +0200

    Update nodeclass with coordinates attibute

--- backend/classes/nodeClass.py
+++ backend/classes/nodeClass.py
@@ -30,7 +30,7 @@ class nodeClass(object):
     deletedNodesList = []   # a list that contains all deleted node IDs
 
     @classmethod
-    def createNode(cls,nodeType):
+    def createNode(cls,nodeType, x=None, y=None):
         ''' this class method dynamically creates instances (new nodes) '''
         if not cls.deletedNodesList: # if there is no deleted nodes
             nodeId=len(nodeClass.nodesDict) # set node ID to the lenght to of the node dictionar
@@ -43,7 +43,7 @@ class nodeClass(object):
             nodeId=cls.deletedNodesList[0] # set node id to the lowest ID (first in sorted)
             cls.deletedNodesList.pop(0) #remove first item in list
 
-        node=nodeClass(nodeId,nodeType) # createa node instance
+        node=nodeClass(nodeId,nodeType,x,y) # createa node instance
         cls.nodesDict[nodeId]=node # append created instance to dictionary
         return node
 
@@ -54,9 +54,11 @@ class nodeClass(object):
         cls.deletedNodesList.append(nodeId) # Append the deleted nodes ID to a list
 
     # Constructor
-    def __init__(self, nodeId, nodeType): # add all the inputs in the init method use if statement if the vars is empty
+    def __init__(self, nodeId, nodeType,x, y): # add all the inputs in the init method use if statement if the vars is empty
         self.nodeId         = nodeId                    # set node ID.
         self.nodeName       = "NODE-"+str(self.nodeId)  # set the default node name to "NODE-nodeID".
+        self.yCoord         = y                         # Y coordinate.
+        self.xCoord         = x                         # X coordinate.
         # Move below to subclasses?
         self.nodeType       = nodeType                  # set node type.
         self.inGoingFlows   = None                      # valid for: destination node.
